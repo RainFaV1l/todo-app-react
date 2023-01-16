5112ebd385b2bad97ae82b113d31da26049d08e6
Azat <Nazmiev_Flesh_Royal@mail.ru>
2023-01-16 14:49:41 +0300

    addingStyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,72 @@
 import { useState, useEffect } from "react";
 
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+
+  return `${day}.${month}.${year}`;
+}
+
 const App = () => {
 
+  // Состоянии (данные) задач
   const [todos, setTodos] = useState([
     {
       id: 1,
-      name: "Купить прудукты",
+      name: "Купить продукты",
       date: new Date(),
       checked: false
     },
     {
       id: 2,
-      name: "Заправить автомобиль",
+      name: "Заправить автомобиль (Lada Granta - чёрного цвета 21 века)",
       date: new Date(),
       checked: false
     }
   ]);
 
+  // Значение поля
   const [value, setValue] = useState('');
 
+  // Функция обновления значения из поля
   const onChangeHandle = (event) => {
     setValue(event.target.value);
   }
 
+  // Функция добавления задачи
   const onSubmitHandle = (event) => {
-
     event.preventDefault();
 
     setTodos((prevState) => {
-
       prevState = [...prevState];
 
       prevState.push({
-        id: Date.now,
+        id: Date.now(),
         name: value,
         date: new Date(),
         checked: false
       });
 
+      return prevState;
     });
 
     setValue('');
   }
 
+  // Функция переключение статуса задачи
   const onCheckedToggle = (id) => {
     setTodos((prevState) => {
       prevState = [...prevState];
 
       prevState = prevState.map((todo) => {
-        if(todo.id === id) {
+        if (todo.id === id) {
           return {
             ...todo,
             checked: !todo.checked
           }
         }
+
         return todo;
       });
 
@@ -61,8 +74,9 @@ const App = () => {
     });
   }
 
-  const onDeleteTodoByid = (id) => {
-    setTodos(() => {
+  // Функция удаления todo из массива по ID
+  const onDeleteTodoById = (id) => {
+    setTodos((prevState) => {
       prevState = [...prevState];
 
       prevState = prevState.filter((todo) => todo.id !== id);
@@ -72,26 +86,38 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div className="layout">
       <div>
         <form onSubmit={(e) => onSubmitHandle(e)}>
           <h2>Добавить задачу:</h2>
-          <input type="text" 
-          placeholder="Купить молоко..." 
-          onChange={(e) => onChangeHandle(e)}
-          value={value}/>
+          <input
+            type="text"
+            placeholder="Купить молоко..."
+            onChange={(e) => onChangeHandle(e)}
+            value={value}
+          />
         </form>
       </div>
-      {/* {Все задачи} */}
-      <div>
+
+      {/* Все задачи */}
+      <div className="allQuestion">
+        {/* Одна задача */}
         {
           todos.map((todo) => {
             return (
               <div>
-                <h3>{todo.name} ({todo.date.toString()})</h3>
+                <h3>{todo.name} ({formatDate(todo.date)})</h3>
                 <div>
-                  <button onClick={() => onCheckedToggle(todo.id)}>{todo.checked ? "Не выполнена" : "Выполнена"}</button>
-                  <button onClick={() => onDeleteTodoByid(todo.id)}>Удалить</button>
+                  <button
+                    onClick={() => onCheckedToggle(todo.id)}
+                  >
+                    {todo.checked ? "Не выполнена" : "Выполнено"}
+                  </button>
+                  <button className="del"
+                    onClick={() => onDeleteTodoById(todo.id)}
+                  >
+                    Удалить
+                  </button>
                 </div>
               </div>
             )
@@ -100,6 +126,6 @@ const App = () => {
       </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
